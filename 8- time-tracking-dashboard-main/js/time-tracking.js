e32363ced1625e4cc68a601e989802e679c4990f
Max Palmer <maxpalmer@gmail.com>
2022-01-03 21:12:06 -0500

    working on populating elements with JS

--- 8- time-tracking-dashboard-main/js/time-tracking.js
+++ 8- time-tracking-dashboard-main/js/time-tracking.js
@@ -1,6 +1,24 @@
 function getTrackingData() {
-    const trackingData = fetch('data.json')
-        .then(data => { return data.JSON() })
-        .then(data => console.log(data));
-    // return trackingData;
+    var trackingData = fetch('data.json')
+        .then(data => {
+            return data.json();
+        })
+        .then(data => {
+            // console.log(data);
+        })
+        .catch(err => {
+            console.log('Could not fetch data!');
+        });
+
 };
+
+function appendData() {
+    var data = getTrackingData(data);
+    const cards = document.querySelectorAll('.card');
+    for (i = 1; i < cards.length; i++) {
+        var title = cards[i].getAttribute('data-flag');
+        console.log(data[]);
+    }
+};
+
+appendData();
