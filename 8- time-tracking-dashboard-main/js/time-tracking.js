c38c5eb448c9ca977e07d9f87e68be6be0195836
aeromax <maxpalmer@gmail.com>
2022-01-05 13:32:33 -0500

    update

--- 8- time-tracking-dashboard-main/js/time-tracking.js
+++ 8- time-tracking-dashboard-main/js/time-tracking.js
@@ -1,6 +1,5 @@
-
-
-function getTrackingData() {
+// Fetch tracking data
+const data = function () {
     const trackingData = fetch('data.json')
         .then(data => {
             return data.json();
@@ -10,19 +9,43 @@ function getTrackingData() {
         });
     return trackingData;
 };
+
+// Apply click events to nav buttons
+(function () {
+    let button = document.querySelectorAll('.time-selection');
+    button.forEach(element => {
+        let time = element.getAttribute('name');
+        element.addEventListener('click', populateElements(time));
+    });
+});
+
+// Gets entries for specific timeframe
+async function getTimeframes(timeframe) {
+
+
+    let t = timeframe;
+    console.log('Getting values for ' + t);
+    for (let i = 0; i < data.length; i++) {
+        let arr = [];
+        arr.push(data[i].timeframes.t);
+        console.log(data[i].timeframes.t);
+    };
+};
+
+
 async function populateElements() {
-    const arr = await getTrackingData();
-    console.log(await getTrackingData());
+    const arr = data;
     let t = '';
     let container = document.querySelector('#grid');
 
     for (let i = 0; i < arr.length; i++) {
+        let card = arr[i];
         let element =
             `
-            < div class="card ${arr[i].title}"> 
+            <div class="card ${(card.title.toLowerCase()).replace(/\s/g, "-")}"> 
             <div class="decoration"></div> 
             <div class="content"> 
-            <div class="title">Work 
+            <div class="title">${card.title}
             <img src="images/icon-ellipsis.svg" width="20" height="auto" alt=""> 
             </div> 
             <div class="current"></div> 
@@ -30,11 +53,8 @@ async function populateElements() {
             </div> 
             </div>
             `;
-        t = arr[i].title;
-        console.log(container);
-        container.innerHTML = element;
+        container.innerHTML += element;
     }
-    return arr;
 };
 
 async function populateData(arr) {
@@ -44,4 +64,5 @@ async function populateData(arr) {
         }
     });
 };
-populateElements();
+
+populateElements(getTimeframes('daily'));
