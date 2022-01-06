61f9db506042e117e45359a4aec23fb29f235f05
aeromax <maxpalmer@gmail.com>
2022-01-06 18:32:27 -0500

    update

--- 8- time-tracking-dashboard-main/js/time-tracking.js
+++ 8- time-tracking-dashboard-main/js/time-tracking.js
@@ -1,74 +1,80 @@
+// Default setting for which period we will show when page loads
+let period = 'daily';
 
 // Store tracking data locally
-let trackingData = getData();
-async function getData() {
-    let response =
-        await fetch('data.json')
-            .then(data => {
-                console.log('getData');
-                return data.json();
-            })
-            .catch(error => {
-                console.log('Could not fetch data!');
-            });
-    let data = await response.data;
-    populateElements();
-
+function getData() {
+    return fetch('data.json')
+        .then(data => {
+            console.log('getData');
+            return data.json();
+        })
+        .then(data => {
+            populateElements(data, period);
+        })
+        .catch(error => {
+            console.log(`Error fetching data: ${error}`);
+        });
 }
 
-// Default setting for which period we will show when page loads
-let period = 'day';
 
-// Apply click events to nav buttons
-(function () {
-    let button = document.querySelectorAll('.time-selection');
-    button.forEach(element => {
-        let time = element.getAttribute('name');
-        element.addEventListener('click', populateElements(time));
-    })
-})
 
-//Gets entries for specific timeframe
-function getTimeframes(period) {
-    console.log('getTimeFrames ' + period);
-    for (let i = 0; i < trackingData.length; i++) {
-        let arr = [];
-        arr.push(trackingData[i].timeframes.period);
-        console.log(trackingData[i].timeframes.period);
+function getTimes(card, period) {
+    let arr = [];
+    if (period == 'daily') {
+        arr.push(card.timeframes.daily);
+        arr[0].previousTimeframe = 'Yesterday';
     }
+    else if (period == 'weekly') {
+        arr.push(card.timeframes.weekly);
+        arr[0].previousTimeframe = 'Last Week';
+    }
+    if (period == 'monthly') {
+        arr.push(card.timeframes.monthly);
+        arr[0].previousTimeframe = 'Last Month';
+    }
+    return arr[0];
 }
 
+//Updates data variables in html
+function populateData(period) {
+    console.log('populateData ' + period);
+}
+// Adds a card for each data entry
 function populateElements(data) {
-
-    console.log('populateElements ' + period);
-    getTimeframes(period);
-    let t = '';
     let container = document.querySelector('#grid');
-    console.log('creating ' + trackingData.length + ' cards');
-    for (let i = 0; i < trackingData.length; i++) {
-        let card = trackingData[i];
+    for (let i = 0; i < data.length; i++) {
+        let card = data[i];
+        let title = card.title;
+        let times = getTimes(card, period);
         let element =
             `
             <div class="card ${(card.title.toLowerCase()).replace(/\s/g, "-")}"> 
             <div class="decoration"></div> 
             <div class="content"> 
-            <div class="title">${card.title}
+            <div class="title">${(title)}
             <img src="images/icon-ellipsis.svg" width="20" height="auto" alt=""> 
             </div> 
-            <div class="current">${card.timeframes.period.current}</div> 
-            <div class="previous"><span class="previous-timeframe"></span><span class="previous-hours"></span></div> 
+            <div class="current">${(times.current)}hrs</div> 
+            <div class="previous">
+            <span class="previous-timeframe">${(times.previousTimeframe)}</span> - <span class="previous-hours">${(times.previous)}hrs</span></div> 
             </div> 
             </div>
             `;
         container.innerHTML += element;
     }
 }
+// Apply click events to nav buttons
+(function (period) {
+    let buttons = document.querySelectorAll('.time-selection');
 
-(function () {
-    if (trackingData.length == 0) {
-        console.log('no data!');
+    for (let i = 0; i < buttons.length; i++) {
+        console.log(buttons[i]);
+        buttons[i].addEventListener('click', function () {
+            console.log('click');
+        });
     }
-    else {
-        console.log('got data!');
-    }
-})
+
+})();
+
+
+getData();
